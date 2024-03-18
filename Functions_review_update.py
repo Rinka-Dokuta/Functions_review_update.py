73ed95a789a8e7f4414c16e55a5420a72ff6f9cd
Rinka <146360541+Rinka-Dokuta@users.noreply.github.com>
2024-03-18 10:10:26 -0600

    Create Functions_review_update.py

---
+++ Functions_review_update.py
@@ -0,0 +1,25 @@
+speed = 0
+time = 0
+product = 0
+length = 0
+width = 0
+
+
+def calculate_distance(speed, time): #EVERYTIME YOU GET PARAMETERS, YOU NEED TO HAVE THEM INSIDE THE PARENTHESES!!
+       
+    product = speed * time
+   
+    print("The distance is", product, "km.")
+   
+
+
+calculate_distance(60,3)
+       
+    
+def calculate_area_of_rectangle(length, width):
+       
+    product = length * width
+   
+    print("The area is", product)
+    
+    
